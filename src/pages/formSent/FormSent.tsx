a205af25549c28ddbb280e30c43e13e5e048acc4
CamilaVerso <camilaverso2@gmail.com>
2024-05-08 15:15:10 -0300

    Ajustado a estilização da página de inscrição concluída

--- src/pages/formSent/FormSent.tsx
+++ src/pages/formSent/FormSent.tsx
@@ -1,12 +1,27 @@
-import './formSent.css';
-import { Link } from 'react-router-dom';
+import "./formSent.css";
+import { Link } from "react-router-dom";
+import send from "../../assets/undraw_Completing_re_i7ap.png"
 
 const FormSent: React.FC = () => {
-    return (
-    <main className='container-formSent'>
-    <h1>Inscrição realizada</h1>
+  return (
+    <main className="container-formSent">
+      <h1>Inscrição realizada</h1>
 
-    <Link to="/"><p className='voltar'>Voltar para a Home</p></Link>
+      <div className="img-g">
+        <img
+          src={send}
+          alt="guincho rebocando um carro"
+          title="Página em Construção"
+        />
+      </div>
+
+      <div className="btn">
+        <Link to="/">
+          <button className="form-login-btn" type="submit">
+            Voltar para a Home
+          </button>
+        </Link>
+      </div>
     </main>
   );
 };
